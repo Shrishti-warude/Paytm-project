72fe472ae69620c74b2c7f91b2bbf15779f3d9ee
Shrishti warude <shrishtiwarude24@gmail.com>
2026-09-14 23:30:39 +0530

    Implemented  signin component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Signup from './pages/Signup';
+import Signin from './pages/Signin';
 
 const App = () => {
 
   return (
    <>
    <Routes>
-   <Route path="/" element={< Signup />} />
+   <Route path="/signup" element={< Signup />} />
    <Route path="/dashboard" element={< Dashboard />} />
+   <Route path="/" element={< Signin/>}/>
    </Routes>
    </>
   )
